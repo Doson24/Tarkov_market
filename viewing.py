6a098503ceaf4f75ece13c7ae6b63e54be5f262d
Doson <karbushev-1991@mail.ru>
2023-01-03 08:23:36 +0700

    Добавил функцию поиска фалйов

--- viewing.py
+++ viewing.py
@@ -1,10 +1,29 @@
+import datetime
+import os
 from collections import OrderedDict
 import pandas as pd
 
 
-filenames = ['data12-07-2022.csv', 'data13-07-2022.csv', 'data28-07-2022.csv', 'data08-08-2022.csv']
-dict_of_df = OrderedDict((f, pd.read_csv(f)) for f in filenames)
+def search_files(path: str) -> list:
+    """
+    Поиск всех .xls файлов в папке
+
+    :param path:
+    :return:
+    """
+
+    list_dir = os.listdir(path)
+    list_files = [i for i in list_dir if '.csv' in i]
+    return list_files
+
+
+filenames = search_files('data')
+
+# При парсинге путает день и месяц
+dict_of_df = OrderedDict(('data/'+f, pd.read_csv('data/'+f, parse_dates=['Date'])) for f in filenames)
+
 df = pd.concat(dict_of_df, sort=True)
-# df = pd.read_csv('data08-08-2022.csv')
-pr = df[df.Name == 'Сошки для HK G36'].price_sell_trader
-print(pr)
+pr = df[df.Name == 'Ключ от номера 220 западного крыла Санатория']
+pr = pr.sort_values('Date')
+
+print(pr.avg_price_24h)
