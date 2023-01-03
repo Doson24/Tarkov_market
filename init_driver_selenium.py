23f8c6f2ecb658a4ac3c8b9c170a38b143188a67
Doson <karbushev-1991@mail.ru>
2023-01-03 08:26:07 +0700

    Добавил автоматическое обновление драйвера

--- init_driver_selenium.py
+++ init_driver_selenium.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def init_webdriver():
     chrome_options = Options()
@@ -15,5 +18,8 @@ def init_webdriver():
     chrome_options.add_argument("user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
                                 " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"'')
     # self.driver = webdriver.Chrome('C:\\install\\chromedriver.exe', options=chrome_options)
-    driver = webdriver.Chrome('./driver/chromedriver.exe', options=chrome_options)
+
+    # driver = webdriver.Chrome('./driver/chromedriver.exe', options=chrome_options)
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     return driver
