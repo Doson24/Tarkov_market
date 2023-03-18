ddab21f59924d64085f8ef0648f1439b0e412e0d
Doson <karbushev-1991@mail.ru>
2023-03-18 23:16:28 +0700

    Не работает нажатие кнопки "Загрузить еще"

--- main.py
+++ main.py
@@ -6,32 +6,53 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from init_driver_selenium import init_webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
 
-def scroll(driver):
-    try:
-        driver.find_element(By.CLASS_NAME, 'big.bold.w-100.text-center.py-10').send_keys(Keys.ENTER)
-    except Exception as ex:
-        print(ex)
-    rows = driver.find_element(By.CLASS_NAME, 'table-list').find_elements(By.CLASS_NAME, 'row')
-    count_rows = len(rows)
+def perform_scroll(driver):
+    el_download = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'big.bold.w-100.text-center.py-10')))
+    # driver.find_element(By.CLASS_NAME, 'big.bold.w-100.text-center.py-10')
+    rows1 = driver.find_element(By.CLASS_NAME, 'table-list').find_elements(By.CLASS_NAME, 'row')
+    count_rows = len(rows1)
 
-    ActionChains(driver) \
-        .scroll_to_element(rows[-1])\
-        .perform()
+    driver.implicitly_wait(2)
 
-    time.sleep(1)
-    while count_rows < len(driver.find_element(By.CLASS_NAME, 'table-list').find_elements(By.CLASS_NAME, 'row')):
-        rows = driver.find_element(By.CLASS_NAME, 'table-list').find_elements(By.CLASS_NAME, 'row')
-        time.sleep(2)
+    # ActionChains(driver) \
+    #     .scroll_to_element(el_download) \
+    #     .click(el_download)\
+    #     .send_keys(Keys.PAGE_DOWN) \
+    #     .perform()
+
+    # el_download.send_keys(Keys.ENTER)
+
+    # ActionChains(driver) \
+    #     .send_keys(Keys.PAGE_DOWN) \
+    #     .perform()
+
+    # rows2 = driver.find_element(By.CLASS_NAME, 'table-list').find_elements(By.CLASS_NAME, 'row')
+    # count_rows = len(rows2)
+    time.sleep(3)
+    driver.find_element(By.CLASS_NAME, 'big.bold.w-100.text-center.py-10').send_keys(Keys.ENTER)
+    # el_download.click()
+
+    time.sleep(3)
+
+    while count_rows < len(get_rows_el(driver)):
+        rows = get_rows_el(driver)
+        count_rows = len(rows)
+        driver.implicitly_wait(2)
+
         ActionChains(driver) \
             .scroll_to_element(rows[-1]) \
             .send_keys(Keys.PAGE_DOWN)\
             .perform()
-        # WebDriverWait(driver, timeout=10).until(document_initialised)
-        count_rows = len(rows)
+
+        driver.implicitly_wait(2)
+
+
+def get_rows_el(driver):
+    return driver.find_element(By.CLASS_NAME, 'table-list').find_elements(By.CLASS_NAME, 'row')
 
 
 def parse_page(driver):
@@ -57,7 +78,7 @@ def parse_page(driver):
         names_trader.append(name_trader)
 
     return pd.DataFrame({
-        'Date': datetime.date.today().strftime('%d-%m-%Y'),
+        'Date': datetime.date.today().strftime('%H-%d-%m-%Y'),
         'Name': names,
         'avg_price_24h': avg_prices24h,
         'change_24h': changes_24h,
@@ -74,20 +95,25 @@ def main(driver):
                   'weapon_parts', 'special_equipment', 'maps', 'ammo_boxes', 'repair']
     driver.set_window_size(1920, 1080)
     data = pd.DataFrame()
+    driver.get('https://tarkov-market.com/ru')
+    time.sleep(6)
     for category in categories:
         driver.get(base_url + category)
-        scroll(driver)
+
+        driver.implicitly_wait(1)
+
+        perform_scroll(driver)
         df = parse_page(driver)
         df['category'] = category
         data = pd.concat([data, df], ignore_index=True)
         print(f'------- download {category} {len(df)} items done -------')
         time.sleep(1)
-    data.to_csv(f'data\data{datetime.date.today().strftime("%d-%m-%Y")}.csv')
+    data.to_csv(f'data\data{datetime.date.today().strftime("%H-%d-%m-%Y")}.csv')
     print(f'count items -  {len(data)}')
 
 
 if __name__ == '__main__':
-    driver = init_webdriver()
+    driver = init_webdriver(headless=False)
     start_time = time.time()
     main(driver)
     driver.quit()
